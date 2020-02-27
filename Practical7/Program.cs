ef577d36ebf9c1d8075c5501b023e8ef67e199be
Chirag Patel <chiragrpatel@live.com>
2020-02-27 10:56:37 +0530

    Practical 7: a.

---
+++ Practical7/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Practical 7: 
+ * Write a C# code to Perform Celsius to Fahrenheit Conversion and Fahrenheit to Celsius conversion. 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float centigrade, fahrenheit;
+            
+            //Take temperature form user
+            Console.Write("Enter temperature in centigrade:  ");
+            centigrade = float.Parse(Console.ReadLine());
+
+            //Convert from centigrade to fahrenheit
+            fahrenheit = centigrade * 9 / 5 + 32;
+            Console.WriteLine($"{centigrade}°C = {fahrenheit}°F");
+            Console.Read();
+        }
+    }
+}
