e8a858b303d5e28d9d269e179f7c6ab264e7eb5a
Chirag Patel <chiragrpatel@live.com>
2020-02-27 08:23:13 +0530

    Practical 5 Till Seven Digits

--- Practical5/Program.cs
+++ Practical5/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿/* Practical 5
+ * Write a C# code to convert digits to words 
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +11,100 @@ namespace Practical5
 {
     class Program
     {
+        static string[] tens = new string[] { "", "", "Twenty", "Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+        static string[] words = new string[] { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+                                            "Eleven", "Twelve", "Thrteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Ninteen"};
+        static string[] denomination = { "Hundred", "Thousand", "Lac", "Crore" };
         static void Main(string[] args)
         {
+            //Declaration and Innitialisation
+            int digits;
+            bool flag = true;
+            string numbeString;
+            string numberInWords = "";
+            
+                      // Take and validate input from the user
+            do
+            {
+                do
+                {
+                    if (flag == false)
+                        Console.WriteLine("Please enter a valid decimal number or 'q' to EXIT");
+                    numbeString = Console.ReadLine();
+                    if (numbeString == "q")
+                        System.Environment.Exit(0);
+                    flag = false;
+                } while (!int.TryParse(numbeString, out digits));
+                numbeString = numbeString.TrimStart('0');
+
+                if (numbeString.Length < 3)
+                    numberInWords = upToTwoDigits(numbeString);
+                else if (numbeString.Length < 4)
+                    numberInWords = threeDigits(numbeString);
+                else if (numbeString.Length < 6)
+                    numberInWords = upToFiveDigits(numbeString);
+                else if (numbeString.Length < 8)
+                    numberInWords = upToSevenDigits(numbeString);
+
+                Console.WriteLine(numberInWords);
+                numberInWords = "";
+            } while (true);
+        }
+
+        private static string upToSevenDigits(string numbeString)
+        {
+            string lacs;
+            string thousands;
+            string hundreds;
+
+            if (numbeString.Length == 6)
+                lacs = upToTwoDigits(numbeString.Substring(0, 1)) + " " + denomination[2];
+            else
+                lacs = upToTwoDigits(numbeString.Substring(0, 2)) + " " + denomination[2];
+
+            thousands = upToTwoDigits(numbeString.Substring(1, 2));
+            if (thousands != "")
+                thousands += " " + denomination[1];
+
+            hundreds = threeDigits(numbeString.Substring(3));
+
+            return $"{lacs} {thousands} {hundreds}";
+        }
+
+        private static string upToFiveDigits(string numbeString)
+        {
+            if (numbeString.Length == 4)
+                return upToTwoDigits(numbeString.Substring(0, 1)) + " " + denomination[1] + " " + threeDigits(numbeString.Substring(1));
+            else
+                return upToTwoDigits(numbeString.Substring(0, 2)) + " " + denomination[1] + " " + threeDigits(numbeString.Substring(2));
+        }
+
+        public static string threeDigits(string numbeString)
+        {
+            int digits = int.Parse(numbeString);
+            if (digits != 0)
+                return words[digits / 100] + " " + (digits/100 == 0?"":denomination[0]) + " " + upToTwoDigits(numbeString.Remove(0, 1));
+            else
+                return "";
+        }
+
+        private static string upToTwoDigits(string numberString)
+        {
+            int digits = int.Parse(numberString);
+           
+            if (digits < 20)
+            {
+                return words[digits];
+            }
+            else if (numberString[1] == '0')
+            {
+                return tens[digits / 10];
+            }
+            else if (digits > 20 && digits < 100)
+            {
+                return tens[digits / 10] + " " + words[digits % 10];
+            }
+            return "";
         }
     }
 }
